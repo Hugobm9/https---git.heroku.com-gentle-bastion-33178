8ba144455df44019dfac2a0b8b4f4c558b4c94c8
Hugobm9 <hugobm9@yahoo.com.br>
2017-12-20 14:54:41 -0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAEqb5EcaQ0BAPwoV4geONk2SC8vbATPYHAcpMdPph5pOe4tdD1b3M8f11cPTbxD41p4VeckghMwvqQcIydr4ZCaZA7n9o6SS3fpZB2jI8gfNUFq6MjzNd684mTohBPyTh9zHT0VyD24jIqZAmvLrEqZBNpRo3EXPLYg6HOjtZAL3zZAnrliFge',
+    FB_VERIFY_TOKEN: 'my_voice_and_my_password-ud-co-bot1',
+    API_AI_CLIENT_ACCESS_TOKEN: 'f6a83aad25394c2697ce4a1228449cb9',
+    FB_APP_SECRET: '481c0b1ff7025efb037b6274c82227c1',
+    SERVER_URL: "https://gentle-bastion-33178.herokuapp.com/",
+};
